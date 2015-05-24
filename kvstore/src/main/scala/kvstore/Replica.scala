d37bee23dd4af5321b262f23c51db4c989fd3556
Jason de Carvalho <jasondecarvalho@gmail.com>
2015-05-24 16:54:15 +0100

    Steps 1 and 2 complete

--- kvstore/src/main/scala/kvstore/Replica.scala
+++ kvstore/src/main/scala/kvstore/Replica.scala
@@ -1,17 +1,7 @@
 package kvstore
 
-import akka.actor.{ OneForOneStrategy, Props, ActorRef, Actor }
+import akka.actor.{Actor, ActorRef, Props}
 import kvstore.Arbiter._
-import scala.collection.immutable.Queue
-import akka.actor.SupervisorStrategy.Restart
-import scala.annotation.tailrec
-import akka.pattern.{ ask, pipe }
-import akka.actor.Terminated
-import scala.concurrent.duration._
-import akka.actor.PoisonPill
-import akka.actor.OneForOneStrategy
-import akka.actor.SupervisorStrategy
-import akka.util.Timeout
 
 object Replica {
   sealed trait Operation {
@@ -33,34 +23,73 @@ object Replica {
 class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
   import Replica._
   import Replicator._
-  import Persistence._
-  import context.dispatcher
 
-  /*
-   * The contents of this actor is just a suggestion, you can implement it in any way you like.
-   */
-  
   var kv = Map.empty[String, String]
   // a map from secondary replicas to replicators
   var secondaries = Map.empty[ActorRef, ActorRef]
   // the current set of replicators
   var replicators = Set.empty[ActorRef]
 
+  var nextSeq = 0
+
+  arbiter ! Join
 
   def receive = {
     case JoinedPrimary   => context.become(leader)
     case JoinedSecondary => context.become(replica)
   }
 
-  /* TODO Behavior for  the leader role. */
   val leader: Receive = {
-    case _ =>
+    case Insert(key, value, id) => {
+      kv += key -> value
+      sender ! OperationAck(id)
+    }
+
+    case Remove(key, id) => {
+      kv -= key
+      sender ! OperationAck(id)
+    }
+
+    case Get(key, id) => {
+      sender ! GetResult(key, kv.get(key), id)
+    }
   }
 
-  /* TODO Behavior for the replica role. */
   val replica: Receive = {
-    case _ =>
+    case Get(key, id) => {
+      sender ! GetResult(key, kv.get(key), id)
+    }
+
+    case Replicate(key, valueOption, id) => {
+      valueOption match {
+        case Some(value) => {
+          kv += key -> value
+          sender ! Replicated(key, id)
+        }
+        case None => {
+          kv -= key
+          sender ! Replicated(key, id)
+        }
+      }
+    }
+
+    case Snapshot(key, valueOption, seq) => {
+      if(seq < nextSeq) {
+        sender ! SnapshotAck(key, seq)
+      } else if(seq == nextSeq) {
+        valueOption match {
+          case Some(value) => {
+            kv += key -> value
+            sender ! SnapshotAck(key, seq)
+          }
+          case None => {
+            kv -= key
+            sender ! SnapshotAck(key, seq)
+          }
+        }
+        nextSeq += 1
+      }
+    }
   }
 
 }
-
